85bb5688de1c9e9e638623726935e68595b7df60
Orekhov Eldar <orekhov.8b@gmail.com>
2025-01-31 01:02:26 +0200

    Homework2

---
+++ Homework2/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ConsoleApp1;
+
+namespace OrderServiceClass;
+
+public class OrderService
+{
+    private readonly ApplicationContext dbContext;
+
+    public OrderService(ApplicationContext db) { dbContext = db; }
+
+    public void AddOrder(List<Product> products, string phone)
+    {
+        Order order = new Order {Phone=phone , Products = products };
+        dbContext.Orders.Add(order);
+        dbContext.SaveChanges();
+        Console.WriteLine($"Order {order.Id} succes");
+    }
+
+    public void DeleteOrder(int orderId)
+    {
+        Order order = dbContext.Orders.Include(o => o.Products).FirstOrDefault(o => o.Id == orderId);
+        if (order != null)
+        {
+            dbContext.Orders.Remove(order);
+            dbContext.SaveChanges();
+            Console.WriteLine($"Order {order.Id} deleted");
+        }
+        else
+            Console.WriteLine($"Order not found");
+    }
+
+    public void ShowOrders()
+    {
+        List<Order> orders = dbContext.Orders.Include(o => o.Products).ToList();
+        foreach (Order order in orders)
+        {
+            Console.WriteLine($"Order {order.Id} for {order.Phone} by ({order.OrderTime}): " + string.Join(", ", order.Products.Select(p => p.Name)));
+        }
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLog;
+using OrderServiceClass;
+
+namespace ConsoleApp1;
+class Program
+{
+    static void Main()
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            OrderService service = new OrderService(db);
+            List<Product> products = new List<Product> 
+            {
+                new Product {Name="Milk", Price=40},
+                new Product {Name="Cheese", Price=70},
+                new Product {Name="Bread", Price=20}
+            };
+            service.AddOrder(products, "+380930000000");
+            service.AddOrder(new List<Product> { products[0], products[2] }, "+380730000000");
+            service.AddOrder(new List<Product> { products[1]}, "+380630000000");
+            service.ShowOrders();
+            service.DeleteOrder(1);
+            service.ShowOrders();
+        }
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order>();
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderTime { get; set; } = DateTime.Now;
+    public string Phone { get; set; }
+    public List<Product> Products { get; set; } = new List<Product>();
+}
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ShopDb;Trusted_Connection=True;");
+    }
+}
